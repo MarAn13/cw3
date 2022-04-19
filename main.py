72c65a3f048c8b13f7e68eeeb28404c22da5c2ba
marAn <maremianin@gmail.com>
2022-04-20 00:47:10 +0300

    added custom media player (styled)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget, \
     QGraphicsOpacityEffect, QVBoxLayout, QLabel, QPushButton, QStyleOption, QStyle, QFileDialog, QGridLayout, \
-    QScrollArea, QMainWindow, QHBoxLayout, QCheckBox, QRadioButton
+    QScrollArea, QMainWindow, QHBoxLayout, QCheckBox, QRadioButton, QSpacerItem
 from PyQt5.QtCore import Qt, QSize, QUrl, QFile, QElapsedTimer, QTimer, QThread, QObject, pyqtSignal
 from PyQt5.QtGui import QLinearGradient, QColor, QBrush, QPalette, QPainter, QPainterPath, QPixmap, QIcon, QCursor, \
     QPen, QFont, QFontMetrics, QImage
@@ -14,30 +14,16 @@ import pyaudio
 import wave
 
 
-# class LogoWidget(QWidget):
-#     def __init__(self, parent=None):
-#         super().__init__(parent=parent)
-#         self.setStyleSheet('QLabel{color: #252525;}')
-#         self.text = QLabel('AVSR', self)
-#         # self.logo_text.setFont(QFont('Arial', 7))
-#         self.text.setAlignment(Qt.AlignCenter)
-#
-#     def resizeEvent(self, e):
-#         self.text.resize(self.geometry().width(), self.geometry().height())
-#
-#     def paintEvent(self, e):
-#         logo_grad = QLinearGradient(self.geometry().width() / 2,
-#                                     0,
-#                                     self.geometry().width() / 2,
-#                                     self.geometry().height()
-#                                     )
-#         logo_grad.setColorAt(0, QColor(18, 249, 255, 255))
-#         logo_grad.setColorAt(1, QColor(224, 176, 255, 255))
-#         logo_painter = QPainter()
-#         logo_painter.setBrush(QBrush(logo_grad))
-#         logo_path = QPainterPath()
-#         logo_path.addRoundedRect(0, 0, self.geometry().width(), self.geometry().height(), 15, 15)
-#         logo_painter.fillPath(logo_path, logo_grad)
+def ms_to_time(milliseconds):
+    secs = int(milliseconds / 1000)
+    mins = int(secs / 60)
+    secs -= mins * 60
+    hours = int(mins / 60)
+    mins -= hours * 60
+    hours, mins, secs = ['0' + i if len(i) == 1 else i for i in [str(hours), str(mins), str(secs)]]
+    return f'{hours}:{mins}:{secs}'
+
+
 def clear_widget(widget):
     for i in widget.children():
         if i.children():
@@ -158,29 +144,97 @@ class AudioProcess(QObject):
         self.mic_state = False
 
 
+class MediaSlider(QSlider):
+    def __init__(self, orientation, parent=None):
+        super().__init__(orientation, parent=parent)
+        self.last_pressed_button = None
+        self.timer = QElapsedTimer()
+
+    def mousePressEvent(self, e):
+        self.last_pressed_button = e.button()
+        if e.button() == Qt.LeftButton:
+            if self.orientation() == Qt.Horizontal:
+                pos = e.localPos().x()
+                move_pos = pos / self.width()
+                move_pos = self.maximum() * move_pos
+            else:
+                pos = self.height() - e.localPos().y()
+                move_pos = pos / self.height()
+                move_pos = self.maximum() * move_pos
+            self.setValue(move_pos)
+            self.timer.restart()
+            self.timer.start()
+
+    def mouseMoveEvent(self, e):
+        if self.last_pressed_button == Qt.LeftButton and self.timer.elapsed() > 20:
+            if self.orientation() == Qt.Horizontal:
+                pos = self.mapFromGlobal(QCursor.pos()).x()
+                move_pos = pos / self.width()
+                move_pos = self.maximum() * move_pos
+            else:
+                pos = self.height() - self.mapFromGlobal(QCursor.pos()).y()
+                move_pos = pos / self.height()
+                move_pos = self.maximum() * move_pos
+            self.setValue(move_pos)
+            self.timer.restart()
+            self.timer.start()
+
+
 class MediaPlayerWidget(QWidget):
     def __init__(self, file, parent=None):
         super().__init__(parent=parent)
         self.setStyleSheet(
             '#area{background-color: #292929; border-radius: 15px}'
-            'QLabel{color: #FFFFFF;}'
-            'QSlider::groove:horizontal{border-radius: 1px; height: 20px; margin: 20px;}'
-            'QSlider::handle{background: none;}'
-            'QSlider::handle::hover{background-color: transparent; border: 4px solid black; height: 22px; width: 22px; margin: -14px 0; border-radius: 14px; padding: -14px 0px;}'
+            'QLabel{color: #FFFFFF; background-color: red;}'
+            'QPushButton{background: yellow;}'
+            '#media_seek{margin: 0px; background: purple;}'
+            '#media_seek::groove:horizontal{border-radius: 5px; height: 18px; margin: 20px 0px 20px 0px; background-color: silver;}'
+            '#media_seek::handle:horizontal{border: 3px solid black; height: 20px; width: 20px; margin: -14px 0; border-radius: 5px; background-color: green;}'
+            '#media_seek::sub-page:horizontal{border-radius: 5px; margin: 20px 0px 20px 0px; background-color: green;}'
+            '#media_volume{margin: 0px;}'
+            '#media_volume::groove:vertical{width: 18px; background-color: silver;}'
+            '#media_volume::handle:vertical{height: 10px; background: black}'
+            '#media_volume::add-page:vertical{background: qlineargradient(x1:0.5, y1:0, x2:0.5, y2:0.25, x3:0.5, y3:1 stop:0 red, stop:0.25 yellow, stop:1 green);}'
         )
+        self.setFocus()
+        self.setFocusPolicy(Qt.StrongFocus)
         self.area = QWidget(parent=self)
         self.area.setObjectName('area')
         self.area_layout = QGridLayout()
         self.process_widget = ProcessWidget(file, False, parent=self.parent())
         self.media_player = QMediaPlayer(flags=QMediaPlayer.VideoSurface, parent=self)
         self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(file)))
+        self.value_change_loop_control = True
         self.media_player_widget = QVideoWidget(parent=self.area)
-        self.control_button = QPushButton(parent=self.area)
-        self.control_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-        self.control_button.clicked.connect(self.toggle_play)
-        self.media_slider = QSlider(Qt.Horizontal, parent=self.area)
+        self.control_button_play = QPushButton(parent=self.area)
+        self.control_button_play.setFocusPolicy(Qt.NoFocus)
+        self.control_button_play.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.control_button_play.clicked.connect(self.toggle_play)
+        self.control_button_stop = QPushButton(parent=self.area)
+        self.control_button_stop.setFocusPolicy(Qt.NoFocus)
+        self.control_button_stop.setIcon(self.style().standardIcon(QStyle.SP_MediaStop))
+        self.control_button_stop.clicked.connect(self.stop)
+        self.control_button_mute = QPushButton(parent=self.area)
+        self.control_button_mute.setFocusPolicy(Qt.NoFocus)
+        self.control_button_mute.setIcon(self.style().standardIcon(QStyle.SP_MediaVolume))
+        self.control_button_mute.clicked.connect(self.toggle_volume)
+        self.media_slider = MediaSlider(Qt.Horizontal, parent=self.area)
+        self.media_slider.setObjectName('media_seek')
+        self.media_slider.setFocusPolicy(Qt.NoFocus)
+        self.media_slider.valueChanged.connect(self.media_slider_value_changed)
+        self.media_volume = MediaSlider(Qt.Vertical, parent=self.area)
+        self.media_volume.setObjectName('media_volume')
+        # self.media_volume.setStyleSheet(
+        #     '#media_volume[cssClass~=muted]::add-page:vertical{opacity: 100}'
+        # )
+        self.media_volume.setFocusPolicy(Qt.NoFocus)
+        self.media_volume.setRange(0, 100)
+        self.media_volume.setValue(self.media_player.volume())
+        self.media_volume.valueChanged.connect(self.media_volume_value_changed)
         self.media_elapsed_time = QLabel('Elapsed', parent=self.area)
+        self.media_elapsed_time.setAlignment(Qt.AlignCenter)
         self.media_remained_time = QLabel('Remained', parent=self.area)
+        self.media_remained_time.setAlignment(Qt.AlignCenter)
         self.media_player.setVideoOutput(self.media_player_widget)
         self.media_player.stateChanged.connect(self.media_player_state_changed)
         self.media_player.positionChanged.connect(self.media_player_position_changed)
@@ -188,12 +242,18 @@ class MediaPlayerWidget(QWidget):
         self.media_elapsed_time.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
         self.media_remained_time.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
         self.media_slider.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Ignored)
-        self.control_button.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
-        self.area_layout.addWidget(self.media_player_widget, 0, 0, 8, 10)
-        self.area_layout.addWidget(self.media_elapsed_time, 8, 0, 1, 1)
-        self.area_layout.addWidget(self.media_slider, 8, 1, 1, 8)
-        self.area_layout.addWidget(self.media_remained_time, 8, 9)
-        self.area_layout.addWidget(self.control_button, 9, 0, 1, 1)
+        self.control_button_play.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.control_button_stop.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.control_button_mute.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.media_volume.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.area_layout.addWidget(self.media_player_widget, 0, 0, 8, 40)
+        self.area_layout.addWidget(self.media_volume, 0, 40, 8, 1)
+        self.area_layout.addWidget(self.media_elapsed_time, 8, 0, 1, 4)
+        self.area_layout.addWidget(self.control_button_play, 8, 4, 1, 4)
+        self.area_layout.addWidget(self.media_slider, 8, 8, 1, 24)
+        self.area_layout.addWidget(self.control_button_stop, 8, 32, 1, 4)
+        self.area_layout.addWidget(self.media_remained_time, 8, 36, 1, 4)
+        self.area_layout.addWidget(self.control_button_mute, 8, 40, 1, 1)
         self.area.setLayout(self.area_layout)
 
     def toggle_play(self):
@@ -202,17 +262,86 @@ class MediaPlayerWidget(QWidget):
         else:
             self.media_player.play()
 
+    def toggle_volume(self):
+        if not self.media_player.isMuted():
+            self.media_player.setMuted(True)
+            self.control_button_mute.setIcon(self.style().standardIcon(QStyle.SP_MediaVolumeMuted))
+            temp = QGraphicsOpacityEffect()
+            temp.setOpacity(0.25)
+            self.media_volume.setGraphicsEffect(temp)
+            # self.media_volume.setProperty('cssClass', 'muted')
+            # self.media_volume.setStyleSheet(self.media_volume.styleSheet())
+        else:
+            self.media_player.setMuted(False)
+            self.control_button_mute.setIcon(self.style().standardIcon(QStyle.SP_MediaVolume))
+            self.media_volume.setGraphicsEffect(None)
+            # self.media_volume.setProperty('cssClass', '')
+            # self.media_volume.setStyleSheet(self.media_volume.styleSheet())
+
+    def stop(self):
+        self.media_player.stop()
+
     def media_player_state_changed(self):
         if self.media_player.state() == QMediaPlayer.PlayingState:
-            self.control_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
+            self.control_button_play.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
         else:
-            self.control_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+            self.control_button_play.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
 
     def media_player_position_changed(self, position):
+        self.value_change_loop_control = False
         self.media_slider.setValue(position)
 
     def media_player_duration_changed(self, duration):
         self.media_slider.setRange(0, duration)
+        self.media_elapsed_time.setText('00:00:00')
+        self.media_remained_time.setText(ms_to_time(duration))
+        font, step = resize_font(self.media_remained_time)
+        self.media_elapsed_time.setFont(font)
+        self.media_remained_time.setFont(font)
+
+    def media_slider_value_changed(self, val):
+        if self.value_change_loop_control:
+            self.media_player.setPosition(val)
+        self.media_elapsed_time.setText(ms_to_time(val))
+        self.value_change_loop_control = True
+
+    def media_volume_value_changed(self, val):
+        self.media_player.setVolume(val)
+
+    def keyPressEvent(self, e):
+        seek_inc, volume_inc = None, None
+        key = e.key()
+        if key == Qt.Key_Left:
+            seek_inc = -10
+        elif key == Qt.Key_Right:
+            seek_inc = 10
+        elif key == Qt.Key_Up:
+            volume_inc = 5
+        elif key == Qt.Key_Down:
+            volume_inc = -5
+        elif key == Qt.Key_Space:
+            self.toggle_play()
+            return
+        else:
+            return
+        if seek_inc is not None:
+            val = self.media_slider.value() + seek_inc * 1000
+            if self.media_slider.minimum() <= val  <= self.media_slider.maximum():
+                self.media_slider.setValue(self.media_slider.value() + seek_inc * 1000)
+            else:
+                if val < self.media_slider.minimum():
+                    self.media_slider.setValue(self.media_slider.minimum())
+                else:
+                    self.media_slider.setValue(self.media_slider.maximum())
+        else:
+            val = self.media_volume.value() + volume_inc
+            if self.media_volume.minimum() <= val <= self.media_volume.maximum():
+                self.media_volume.setValue(self.media_volume.value() + volume_inc)
+            else:
+                if val < self.media_volume.minimum():
+                    self.media_volume.setValue(self.media_volume.minimum())
+                else:
+                    self.media_volume.setValue(self.media_volume.maximum())
 
     def resizeEvent(self, e):
         self.area.setFixedSize(self.width(), self.height())
@@ -390,13 +519,8 @@ class VideoRecordWidget(QWidget):
     def update_time(self):
         if not self.record_toggle_button.isEnabled():
             self.record_toggle_button.setEnabled(True)
-        secs = int(self.record_timer.elapsed() / 1000)
-        mins = int(secs / 60)
-        secs -= mins * 60
-        hours = int(mins / 60)
-        mins -= hours * 60
-        hours, mins, secs = ['0' + i if len(i) == 1 else i for i in [str(hours), str(mins), str(secs)]]
-        self.record_timer_text.setText(f'{hours}:{mins}:{secs}')
+        time_str = ms_to_time(self.record_timer.elapsed())
+        self.record_timer_text.setText(time_str)
 
     def destroy(self):
         if self.worker_video:
@@ -854,7 +978,9 @@ def main():
     # QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)  # enable high dpi scaling
     # QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)  # use high dpi icons
     app = QApplication([])
-    window = MainWindow()
+    # window = MainWindow()
+    window = MediaPlayerWidget('test.mp4')
+    window.setFixedSize(1200, 800)
     window.show()
     app.exec_()
     print('Finished')
